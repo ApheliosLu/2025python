864d30de34284508387a9ce77a70186e2b963be4
ApheliosLu <1209706925@qq.com>
2025-01-22 13:16:54 +0800

    day24

--- BusinessCardManagementSystem/cards_main.py
+++ BusinessCardManagementSystem/cards_main.py
@@ -2,3 +2,26 @@
 # 2025年01月04日19时31分30秒
 # Leon12097@163.com
 
+# 主程序代码
+
+import cards_tools
+
+while True:
+    # TODO(小明) 显示系统菜单
+    cards_tools.show_menu()
+    action = input("请选择操作功能：")
+    print("您选择的操作是：%s" % action)
+
+    # 根据用户输入决定后续的操作
+    if action in ["1", "2", "3"]:
+        if action == "1":
+            cards_tools.new_card()
+        elif action == "2":
+            cards_tools.show_all()
+        elif action == "3":
+            cards_tools.search_card()
+    elif action == "0":
+        print("欢迎再次使用【名片管理系统】")
+        break
+    else:
+        print("输入错误，请重新输入")
---
+++ BusinessCardManagementSystem/cards_tools.py
@@ -0,0 +1,47 @@
+# 作者: 陆离ApheliosLu
+# 2025年01月22日08时09分31秒
+# Leon12097@163.com
+
+# 所有名片功能函数
+
+
+def show_menu():
+    """
+    显示菜单
+    """
+    print("*" * 50)
+    print("欢迎使用【菜单管理系统】V1.0")
+    print("")
+    print("1. 新建名片")
+    print("2. 显示全部")
+    print("3. 查询名片")
+    print("")
+    print("0. 退出系统")
+    print("*" * 50)
+
+
+def new_card():
+    """
+    新建名片
+    """
+
+    print("-" * 50)
+    print("功能：新建名片")
+
+
+def show_all():
+    """
+    显示全部
+    """
+
+    print("-" * 50)
+    print("功能：显示全部")
+
+
+def search_card():
+    """
+    搜索名片
+    """
+
+    print("-" * 50)
+    print("功能：搜索名片")
